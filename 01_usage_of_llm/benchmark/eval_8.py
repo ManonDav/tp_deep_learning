ba0110d346916d0ff9d5f9f7fc27f8316b6edf4e
Davion <manon.davion@gmail.com>
2026-09-15 11:39:54 +0200

    Script pour vérifier si la fonction générée passe les tests pour le 8e cas

---
+++ 01_usage_of_llm/benchmark/eval_8.py
@@ -0,0 +1,27 @@
+import evaluator
+
+
+task = evaluator.get_task_description("08_roman_to_int")
+implementation= evaluator.test_implementation("08_roman_to_int", """def roman_to_int(s: str) -> int:
+    roman_to_value = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    prev_value = 0
+
+    for char in reversed(s):
+        value = roman_to_value[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total""")
+print(implementation)
