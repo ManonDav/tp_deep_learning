2d44c913c71e8ff23328c60691f08ea8896833ce
Davion <manon.davion@gmail.com>
2026-09-15 11:26:22 +0200

    Script pour vérifier si la fonction générée passe les tests pour le 4e cas

---
+++ 01_usage_of_llm/benchmark/04_most_frequent/solution.py
@@ -0,0 +1,15 @@
+def most_frequent(xs):
+    frequency = {}
+    max_freq = 0
+    most_freq_element = None
+
+    for element in xs:
+        frequency[element] = frequency.get(element, 0) + 1
+
+        if frequency[element] > max_freq:
+            max_freq = frequency[element]
+            most_freq_element = element
+        elif frequency[element] == max_freq and most_freq_element is None:
+            most_freq_element = element
+
+    return most_freq_element
---
+++ 01_usage_of_llm/benchmark/eval_4.py
@@ -0,0 +1,20 @@
+import evaluator
+
+
+task = evaluator.get_task_description("04_most_frequent")
+implementation= evaluator.test_implementation("04_most_frequent", """def most_frequent(xs):
+    frequency = {}
+    max_freq = 0
+    most_freq_element = None
+
+    for element in xs:
+        frequency[element] = frequency.get(element, 0) + 1
+
+        if frequency[element] > max_freq:
+            max_freq = frequency[element]
+            most_freq_element = element
+        elif frequency[element] == max_freq and most_freq_element is None:
+            most_freq_element = element
+
+    return most_freq_element""")
+print(implementation)
